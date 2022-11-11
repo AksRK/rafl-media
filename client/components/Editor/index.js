f1511714d755f206ea58a3ae589748ff6dd46219
Alex Romanchuk <alexandr.romanchuk96@gmail.com>
2022-11-11 21:06:39 +0300

    Добавлена загрузка картинки на сервер

--- client/components/Editor/index.js
+++ client/components/Editor/index.js
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import axios from "axios";
 
 const SunEditor = dynamic(() => import("suneditor-react"), {
     ssr: false
@@ -19,7 +20,8 @@ export default function Editor({ initialContent = '', name, onChange, props, con
         "imageResizing": false,
         "imageHeightShow": false,
         "imageAlignShow": false,
-
+        // "imageUploadUrl": "http://localhost:4444/uploads",
+        // "imageMultipleFile": true,
         buttonList: [
             [
                 'undo',
@@ -47,6 +49,53 @@ export default function Editor({ initialContent = '', name, onChange, props, con
         ],
     }
 
+    const handleImageUploadBefore = (files, info, uploadHandler) => {
+        const Data = new FormData()
+        Data.append('image', files[0])
+
+        axios.post(
+            'http://localhost:4444/uploads',
+            Data,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }).then((response) => {
+            // console.log(response)
+            const res = {
+                // The response must have a "result" array.
+                errorMessage: response?.data?.message,
+                result: [
+                    {
+                        url: response.data.fullUrl,
+                        size: response.data.file_size,
+                        name: response.data.public_id
+                    }
+                ]
+            }
+            uploadHandler(res)
+        })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    const handleImageUpload = (
+        targetElement,
+        index,
+        state,
+        info,
+        remainingFilesCount,
+        core,
+    ) => {
+        console.log(targetElement.src)
+        console.log(core)
+    }
+
+    const handleImageUploadError = (errorMessage, result) => {
+        console.log(errorMessage, result)
+    }
+
     return (
         <div>
             <SunEditor
@@ -58,6 +107,9 @@ export default function Editor({ initialContent = '', name, onChange, props, con
                 setContents={initialContent}
                 setDefaultStyle={'font-family: Inter;'}
                 setOptions={options}
+                onImageUploadBefore={handleImageUploadBefore}
+                onImageUpload={handleImageUpload}
+                onImageUploadError={handleImageUploadError}
                 onChange={onChange}
             />
         </div>
