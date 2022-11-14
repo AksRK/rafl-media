c295e55f0ed6903cc5ac5daf5b5c99513c8189ee
Alex Romanchuk <alexandr.romanchuk96@gmail.com>
2022-11-14 21:46:22 +0300

    Добавлен запрос на добавление лайков на стороне серва

--- client/components/UI/Liked/index.js
+++ client/components/UI/Liked/index.js
@@ -1,16 +1,23 @@
 import styles from './index.module.scss'
 import {useEffect, useState} from "react";
+import axios from "axios";
 
-function Liked({likes}) {
-    const [likeCount, setLikeCount] = useState(likes)
+function Liked({likes, postId, typePost = 'default'}) {
+    const [likesTotal, setLikesTotal] = useState(likes)
+    const [likeCount, setLikeCount] = useState(0)
     const [btnState, setBtnState] = useState(false)
     const [clicked, setClicked] = useState(false)
-    const [startAnimThx, setStartAnimThx] = useState(false)
 
     const liked = () => {
         if (!clicked && likeCount <= 4) {
             setClicked(true)
             setLikeCount(likeCount + 1)
+            setLikesTotal(likesTotal + 1)
+
+            axios.put(`/api${typePost === 'creator'?'/creator':''}/posts/like/${postId}`,
+            ).catch((error) => {
+                console.log(error)
+            })
         }
     }
 
@@ -30,13 +37,12 @@ function Liked({likes}) {
         }
     }, [likeCount])
 
-
     return (
         <button disabled={btnState} onClick={!clicked?liked:()=>{}} className={styles.like}>
             {
                 clicked?
                     <> <span></span> <span className={styles.like__count}>{'+'+likeCount}</span></>:
-                    <> <span></span> <span className={styles.like__total}>{likes}</span></>
+                    <> <span></span> <span className={styles.like__total}>{likesTotal}</span></>
             }
 
             {
