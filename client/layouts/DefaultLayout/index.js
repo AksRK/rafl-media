17cae881eebca34282eba34675f6f2ae4df873d4
Alex Romanchuk <alexandr.romanchuk96@gmail.com>
2022-11-16 14:39:14 +0300

    Кривое Нечто с анимацией контейнера

--- client/layouts/DefaultLayout/index.js
+++ client/layouts/DefaultLayout/index.js
@@ -2,6 +2,7 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import CarouselBanner from "../../components/CarouselBanner";
 import {useRouter} from "next/router";
+import {useScroll} from "../../core/hooks/useScroll";
 
 export const publicRoutes = [
     {link: '/', name: 'Медиа'},
@@ -12,20 +13,27 @@ export const publicRoutes = [
 
 export default function DefaultLayout({children, bannerState=true}) {
     const router = useRouter()
-
+    const {scrollX, scrollY} = useScroll()
     return (
         <div className={'container'}>
-            <Header routes={publicRoutes}/>
-            {
-                router.asPath.includes('contacts')
-                ||  router.asPath.includes('user-license-agreement')
-                ||  router.asPath.includes('about-project')
-                ||  router.asPath.includes('posts')
-                    ? null
-                    : <CarouselBanner/>
-            }
-            {children}
-            <Footer/>
+            <div className={'head' + (scrollY >= 450?' head_hidden':'')}>
+                <Header routes={publicRoutes}/>
+                {
+                    router.asPath.includes('contacts')
+                    ||  router.asPath.includes('user-license-agreement')
+                    ||  router.asPath.includes('about-project')
+                    ||  router.asPath.includes('posts')
+                        ? null
+                        : <CarouselBanner/>
+                }
+            </div>
+
+            <div className={'body'+ (bannerState?' body_with_banner':'')}>
+                {children}
+            </div>
+            <div style={scrollY <= 450 && bannerState?{opacity:0}:{}}>
+                <Footer/>
+            </div>
         </div>
     )
 }
