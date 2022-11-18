0a280307c185243fa39a61878b404c70e3ae5506
Alex Romanchuk <alexandr.romanchuk96@gmail.com>
2022-11-18 15:51:20 +0300

    добавлен разделитель для моб версии карточек креаторов

--- client/components/Card/index.js
+++ client/components/Card/index.js
@@ -16,56 +16,61 @@ function Card({type, title, description, imgUrl, tag = null, path}) {
     </svg>
 
     return (
-        <Link href={path}
-              // scroll={false}
-              className={styles.card + ' ' + (type === 'creator' ? styles.card_creator : '') + ' ' +
-                  (mobile >= size.width ? styles.card_mob : '')
-              }>
-            <div className={styles.card__img + ' ' + (type === 'creator' ? styles.card__img_creator : '')}>
-                <Image src={imgUrl} alt={title} width={378} height={512}/>
-                {
-                    type !== 'creator' && tag ?
-                        <div className={styles.card__tag}>{Category.find((c) => c.value === tag).label}</div> :
-                        ''
-                }
-                {
-                    type !== 'creator' && tag
-                        ? <div onClick={(event) => {
-                            event.preventDefault()
-                            router.push(`/${tag}#main`, null, {scroll: false})
-                        }
-                        } className={styles.card__tag}>
-                            {Category.find((c) => c.value === tag).label}
-                        </div> :
+        <>
+            <Link href={path}
+                // scroll={false}
+                  className={styles.card + ' ' + (type === 'creator' ? styles.card_creator : '') + ' ' +
+                      (mobile >= size.width ? styles.card_mob : '')
+                  }>
+                <div className={styles.card__img + ' ' + (type === 'creator' ? styles.card__img_creator : '')}>
+                    <Image src={imgUrl} alt={title} width={378} height={512}/>
+                    {
+                        type !== 'creator' && tag ?
+                            <div className={styles.card__tag}>{Category.find((c) => c.value === tag).label}</div> :
+                            ''
+                    }
+                    {
+                        type !== 'creator' && tag
+                            ? <div onClick={(event) => {
+                                event.preventDefault()
+                                router.push(`/${tag}#main`, null, {scroll: false})
+                            }
+                            } className={styles.card__tag}>
+                                {Category.find((c) => c.value === tag).label}
+                            </div> :
 
-                        ''
-                }
+                            ''
+                    }
 
-            </div>
-            <div className={styles.card__wrp + ' ' + (type === 'creator' ? styles.card__wrp_creator : '')}>
-                <h2 className={styles.card__title + ' ' + (type === 'creator' ? styles.card__title_creator : '')}>
-                    <ShrinkText text={title.replace(':', ':\n')} maxChar={39}/>
-                </h2>
+                </div>
+                <div className={styles.card__wrp + ' ' + (type === 'creator' ? styles.card__wrp_creator : '')}>
+                    <h2 className={styles.card__title + ' ' + (type === 'creator' ? styles.card__title_creator : '')}>
+                        <ShrinkText text={title.replace(':', ':\n')} maxChar={39}/>
+                    </h2>
 
-                <span
-                    className={styles.card__description + ' ' + (type === 'creator' ? styles.card__description_creator : '')}>
+                    <span
+                        className={styles.card__description + ' ' + (type === 'creator' ? styles.card__description_creator : '')}>
                      <ShrinkText text={description.replace(':', ':\n')} maxChar={type==='creator'?mobile >= size.width ? 60 : 122:mobile >= size.width ? 185 : 122}/>
                 </span>
-                {/*Основатель RAFL.MEDIA, коллекционер фубольных*/}
-                {/*джерси.*/}
-                <div className={styles.card__readBtn}>
-                    {
-                        type === 'creator' && mobile <= size.width ?
-                            <>Смотреть статьи {arrow}</> : ''
-                    }
-                    {
-                        type !== 'creator' ?
-                            <>Читать {arrow}</> : ''
-                    }
-                </div>
+                    {/*Основатель RAFL.MEDIA, коллекционер фубольных*/}
+                    {/*джерси.*/}
+                    <div className={styles.card__readBtn}>
+                        {
+                            type === 'creator' && mobile <= size.width ?
+                                <>Смотреть статьи {arrow}</> : ''
+                        }
+                        {
+                            type !== 'creator' ?
+                                <>Читать {arrow}</> : ''
+                        }
+                    </div>
 
-            </div>
-        </Link>
+                </div>
+            </Link>
+            {
+                type === 'creator' && mobile >= size.width?<div className={styles.separator}></div>:''
+            }
+        </>
     )
 }
 
