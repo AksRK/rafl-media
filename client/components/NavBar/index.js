57f37a0992850a796341a4632fce7fb144f4b1e1
Alexandr Romanchuk <alexandr.romanchuk96@gmail.com>
2022-12-04 07:34:38 +0200

    исправлен баг при клике на активную ссылку

--- client/components/NavBar/index.js
+++ client/components/NavBar/index.js
@@ -2,26 +2,42 @@ import React from 'react';
 import styles from "../Header/index.module.scss";
 import NavLink from "../UI/NavLink";
 
-const NavBar = ({routes, setBurgerStateZIndex}) => {
+const NavBar = ({routes, setBurgerStateZIndex, setBurgerState}) => {
     return (
         <nav className={styles.headerNav}>
-            {routes.map(r =>
-                <NavLink
-                    key={r.link}
-                    href={r.link}
-                    handleClick={() => {
-                        if (r.handleClick) {
-                            r.handleClick()
-                        }
-                        document.querySelector('body').style.overflow = 'visible'
-                        setTimeout(() => {
-                            setBurgerStateZIndex('2')
-                        }, 700)
-                    }}
-                    myClassName={styles.headerNav__link}
-                    myActiveClassName={styles.headerNav__link_active}>
-                    <span>{r.name}</span>
-                </NavLink>
+            {routes.map(r => {
+                if (r.type === 'external') {
+                    return (
+                        <a key={r.link}
+                           href={r.link}
+                           target="_blank"
+                           rel="nofollow noopener noreferrer"
+                           className={styles.headerNav__link}>
+                            <span>{r.name}</span>
+                        </a>
+                    )
+                }else {
+                    return (
+                        <NavLink
+                            key={r.link}
+                            href={r.link}
+                            handleClick={() => {
+                                if (r.handleClick) {
+                                    r.handleClick()
+                                }
+                                document.querySelector('body').style.overflow = 'visible'
+                                setBurgerState(false)
+                                setTimeout(() => {
+                                    setBurgerStateZIndex('2')
+                                }, 700)
+                            }}
+                            myClassName={styles.headerNav__link}
+                            myActiveClassName={styles.headerNav__link_active}>
+                            <span>{r.name}</span>
+                        </NavLink>
+                    )
+                }
+                }
             )}
         </nav>
     );
