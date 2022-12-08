f1b13050d3164dbf0146ff3c71972af0d942675a
Alex Romanchuk <alexandr.romanchuk96@gmail.com>
2022-12-09 01:25:38 +0300

    Исправлен баг отображения текста

--- client/components/Footer/index.js
+++ client/components/Footer/index.js
@@ -10,69 +10,71 @@ function Footer({styled = {}}) {
     return (
         <footer className={style.footer + ' container'} style={styled}>
 
-            <div className={style.footer__logo}>
-                <Link href={'/'}>
-                    <Image src={footerLogo} alt={'RAFL'}/>
-                </Link>
+            <div className={style.footer__wrp}>
+                <div className={style.footer__logo}>
+                    <Link href={'/'}>
+                        <Image src={footerLogo} alt={'RAFL'}/>
+                    </Link>
 
-            </div>
-            <div className={style.footerLinks}>
-               <div className={style.footerLinks__el}>
-                   <h5 className={style.footerLinks__title}>
-                       Меню
-                   </h5>
-                   <NavLink
-                       href={'/'}
-                       myClassName={'link'}
-                       myActiveClassName={' link_active'}>
-                       <span>Медиа</span>
-                   </NavLink>
-                   <a className={'link'} href="http://rafl.studio/" target="_blank" rel="noreferrer"><span>Студия</span></a>
-                   <NavLink
-                       href={'/about-project'}
-                       myClassName={'link'}
-                       myActiveClassName={' link_active'}>
-                       <span>О проекте</span>
-                   </NavLink>
-                   <NavLink
-                       href={'/contacts'}
-                       myClassName={'link'}
-                       myActiveClassName={' link_active'}>
-                       <span>Контакты</span>
-                   </NavLink>
-               </div>
-                <div className={style.footerLinks__el}>
-                    <h5 className={style.footerLinks__title}>
-                        Поддержка
-                    </h5>
-                    <NavLink
-                        href={'/user-license-agreement'}
-                        myClassName={'link link_column'}
-                        myActiveClassName={' link_active'}>
-                        <span>Пользовательское</span>
-                        <span>соглашение</span>
-                    </NavLink>
+                </div>
+                <div className={style.footerLinks}>
+                    <div className={style.footerLinks__el}>
+                        <h5 className={style.footerLinks__title}>
+                            Меню
+                        </h5>
+                        <NavLink
+                            href={'/'}
+                            myClassName={'link'}
+                            myActiveClassName={' link_active'}>
+                            <span>Медиа</span>
+                        </NavLink>
+                        <a className={'link'} href="http://rafl.studio/" target="_blank" rel="noreferrer"><span>Студия</span></a>
+                        <NavLink
+                            href={'/about-project'}
+                            myClassName={'link'}
+                            myActiveClassName={' link_active'}>
+                            <span>О проекте</span>
+                        </NavLink>
+                        <NavLink
+                            href={'/contacts'}
+                            myClassName={'link'}
+                            myActiveClassName={' link_active'}>
+                            <span>Контакты</span>
+                        </NavLink>
+                    </div>
+                    <div className={style.footerLinks__el}>
+                        <h5 className={style.footerLinks__title}>
+                            Поддержка
+                        </h5>
+                        <NavLink
+                            href={'/user-license-agreement'}
+                            myClassName={'link link_column'}
+                            myActiveClassName={' link_active'}>
+                            <span>Пользовательское</span>
+                            <span>соглашение</span>
+                        </NavLink>
+                    </div>
+
+                    <div className={style.footerLinks__social}>
+                        <SocialLink url={'https://www.instagram.com/rafl.media/'} black={false}/>
+                        <SocialLink url={'https://t.me/raflmedia'} black={false}/>
+                        <SocialLink url={'https://vk.com/raflmedia'} black={false}/>
+                        <SocialLink url={'https://www.youtube.com/channel/UC5qMpkZ_Di6whW9C88TF8Tw/videos'} black={false}/>
+                    </div>
                 </div>
 
-                <div className={style.footerLinks__social}>
-                    <SocialLink url={'https://www.instagram.com/rafl.media/'} black={false}/>
-                    <SocialLink url={'https://t.me/raflmedia'} black={false}/>
-                    <SocialLink url={'https://vk.com/raflmedia'} black={false}/>
-                    <SocialLink url={'https://www.youtube.com/channel/UC5qMpkZ_Di6whW9C88TF8Tw/videos'} black={false}/>
-                </div>
-            </div>
-
-            <div className={style.footer__extra}>
-                Мы расстраиваемся, когда кто-то
-                копирует и распространяет материалы сайта без ссылки на источник.
-            </div>
-
-            <div className={style.footer__copyright}>
-                <div className={style.footer__copyright__el}>
-                    Сайт задизайнен и создан
-                    <a className={'link link_white'} href="rafl-media-backend/сlient/components/Footer/index#"><span>beans</span></a>
+                <div className={style.footer__extra}>
+                    Мы расстраиваемся, когда кто-то
+                    копирует и распространяет материалы сайта без ссылки на источник.
                 </div>
 
+                <div className={style.footer__copyright}>
+                    <div className={style.footer__copyright__el}>
+                        Сайт задизайнен и создан
+                        <a className={'link link_white'} href="rafl-media-backend/сlient/components/Footer/index#"><span>beans</span></a>
+                    </div>
+
+                </div>
             </div>
 
         </footer>
