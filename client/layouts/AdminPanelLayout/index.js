56d60dd4d10b1582527cfef58e46c97338f64d2c
Alex Romanchuk <alexandr.romanchuk96@gmail.com>
2022-12-09 01:22:05 +0300

    Добавлен линк на страницу редактирования Контактов

--- client/layouts/AdminPanelLayout/index.js
+++ client/layouts/AdminPanelLayout/index.js
@@ -2,7 +2,6 @@ import styles from './styles.module.scss'
 import Header from "../../components/Header";
 import {useContext, useState} from "react";
 import {AuthContext} from "../../pages/_app";
-import {publicRoutes} from "../DefaultLayout";
 
 function AdminPanelLayout({children}) {
     const {isAuth, setIsAuth} = useContext(AuthContext)
@@ -13,6 +12,7 @@ function AdminPanelLayout({children}) {
         {link: '/admin/panel/users', name: 'Пользователи'},
         {link: '/admin/panel/posts', name: 'Статьи'},
         {link: '/admin/panel/creators', name: 'Креаторы'},
+        {link: '/admin/panel/contacts', name: 'Контакты'},
         {
             link: '/', name: 'Выход', handleClick: () => {
                 localStorage.removeItem('token')
