61d3423825e9a6ca6168bc4a5559b0a3a68c68d8
Alex Romanchuk <alexandr.romanchuk96@gmail.com>
2022-12-27 19:14:30 +0300

    удалил встроенное превью

--- client/components/Editor/index.js
+++ client/components/Editor/index.js
@@ -45,7 +45,6 @@ export default function Editor({ initialContent = '', name, onChange, props, con
                 "video",
                 'showBlocks',
                 'codeView',
-                'preview'
             ]
         ],
     }
