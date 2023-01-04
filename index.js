a3effee040388ecedd1a390fff235ecb5ce127bd
Alex Romanchuk <alexandr.romanchuk96@gmail.com>
2023-01-04 16:33:14 +0300

    удалены комментарии

--- index.js
+++ index.js
@@ -139,10 +139,3 @@ async function start() {
 
 start()
 
-// app.listen(4444, (err)=> {
-//     if (err) {
-//         return console.log(err)
-//     }
-//
-//     console.log('Server running')
-// })
